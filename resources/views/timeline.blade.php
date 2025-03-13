96e6118821e1607ad50e76bfb07b46baa5927793
Supan Adit Pratama <account@supanadit.com>
2025-03-13 15:52:07 +0000

    feat: dependency gantt sample

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -38,12 +38,21 @@
                 end: '2025-03-16',
                 progress: 20
             },
+            {
+                id: '2',
+                name: 'Redesign website',
+                start: '2025-03-03',
+                end: '2025-03-04',
+                progress: 20,
+                dependencies: '1'
+            }
         ];
         let gantt = new Gantt("#gantt", tasks, {
             bar_height: 16,
             column_width: 30,
             container_height: 670,
             view_mode_select: true,
+            readonly: true,
         });
     </script>
 @endsection
